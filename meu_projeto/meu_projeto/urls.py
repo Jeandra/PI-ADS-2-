76a9d6b8c07a820d11e0993d6b1394912ab5f9e8
Jeandra <jeandra8789@gmail.com>
2024-03-31 13:58:04 -0300

    Update urls.py

--- meu_projeto/meu_projeto/urls.py
+++ meu_projeto/meu_projeto/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from meusapp import views
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('listar/', views.listar_ptodutos, name='listar_ptodutos'),
     path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
